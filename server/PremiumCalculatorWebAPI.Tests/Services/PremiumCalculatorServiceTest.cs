a174159135eaed9b1aed4a0144b1c792c92252e8
faslurahmankv@gmail.com <faslurahmankv@gmail.com>
2021-06-24 00:56:36 +1000

    New Premium Calculator committing
    New Premium Calculator committing

---
+++ server/PremiumCalculatorWebAPI.Tests/Services/PremiumCalculatorServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PremiumCalculatorWebAPI.Controllers;
+using  Microsoft.VisualStudio.TestTools.UnitTesting;
+using PremiumCalculatorWebAPI.Domain.Services;
+
+namespace PremiumCalculatorWebAPI.Tests.Services
+{
+    [TestClass]
+    class PremiumCalculatorServiceTest
+    {
+        
+        private static double GetPremium(int age, double amount, string occupation)
+        {
+            var service = new PremiumController(new PremiumService());
+            var result = service.CalculatePremium(age, amount, occupation);
+            return Convert.ToDouble(result.Result);
+        }
+        
+        [TestMethod]
+        public void CalculatePremium_with_Null_Inputs()
+        {
+            try
+            {
+                var result = GetPremium(0, 0, null);
+                Assert.Fail();
+            }
+            catch { 
+            
+            }
+        }
+
+        public void CalculatePremium_with_Invalide_Occupation()
+        {
+            try
+            {
+                var result = GetPremium(0, 0, "Testoccupation");
+            }
+            catch { }
+        }
+
+    }
+}
